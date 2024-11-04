380311f6bebf722c01f83aecd8751e20a1495c60
Lorenzo Tarabelli <tarabelli38@gmail.com>
2024-11-04 15:19:15 +0100

    Initial Commit

---
+++ MyAmazingApp/MyAmazingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAmazingAppApp.swift
+//  MyAmazingApp
+//
+//  Created by Lorenzo Tarabelli on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MyAmazingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
